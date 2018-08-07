53deeca07b80c805e58d2de2b94331f2dd97851e
navybluesilver <navybluesilver@protonmail.ch>
2018-08-07 15:27:35 +0700

    adding config path based on HOME

--- dlcexchange/config/config.go
+++ dlcexchange/config/config.go
@@ -8,6 +8,7 @@ import (
 func GetString(config string) (s string) {
 	viper.SetConfigName("dlcexchange") // name of config file (without extension)
 	viper.AddConfigPath("/home/user/dlcexchange/alice")
+	viper.AddConfigPath("$HOME/dlcexchange/config")
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
