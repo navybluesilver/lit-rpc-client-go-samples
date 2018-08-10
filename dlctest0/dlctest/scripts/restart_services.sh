810d02579b0212a98eb1cdc5a3ac2a2dd2974b3c
navybluesilver <navybluesilver@protonmail.ch>
2018-08-10 17:04:27 +0700

    remove backup folder

--- dlctest0/dlctest/scripts/restart_services.sh
+++
@@ -1,5 +0,0 @@
-echo "restart service lit.alice.service"
-systemctl restart lit.alice.service 
-echo "restart service lit.bob.service"
-systemctl restart lit.bob.service 
-
--- dlctest0/dlctest/scripts/startup_bob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $GOPATH/src/github.com/mit-dci/lit-rpc-client-go-samples/dlctest
-./bin/lit --dir=bob -v
